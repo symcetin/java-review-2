143045b69881caff728f3aa2a2481d2dad3306c8
jamaldemir <jamal@cybertekschool.com>
2022-07-30 10:55:23 -0400

    review classes created

--- generics/src/main/java/review/GenericTest.java
+++ generics/src/main/java/review/GenericTest.java
@@ -12,16 +12,21 @@ public class GenericTest {
         studentList.add(new Student("Mike",101));
         studentList.add(new Student("Joe",102));
         studentList.add(new Student("Adam",103));
+        studentList.add(new Student("Ozzy",103));
 
         List<Teacher> teacherList = new ArrayList<>();
 
         teacherList.add(new Teacher("Albus",201));
         teacherList.add(new Teacher("Severus",202));
+        teacherList.add(new Teacher("Hagrid",202));
 
         //printInfo(studentList);
 
         printInfos(studentList);
         printInfos(teacherList);
+
+        System.out.println(getLastItem(studentList).getName());
+        System.out.println(getLastItem(teacherList).getName());
     }
 
     //write a method takes list of students, print each student and total count
@@ -46,5 +51,10 @@ public class GenericTest {
 
     }
 
+    //write a method that takes a list, and return the last item of the list.
+    public static <T> T getLastItem(List<T> list){
+
+        return list.get(list.size()-1);
+    }
 
 }
